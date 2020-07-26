4f5d754f9d6c283ecad5929e7448f6a034c4db31
Helen <hele.suarez@unah.hn>
2020-07-26 02:32:03 -0600

    Examen

---
+++ EjerciciosDevC++/Examen01/main.cpp
@@ -0,0 +1,45 @@
+#include <cstring>
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	int c;
+	int ciclos =0;
+ 	string password;
+ 	
+    cout << "Eres un agente? Ingresa tu password para acceder a la matrix: ";
+    cin >>  password;
+    if (password=="smith"||password=="SMITH")
+	{
+	    system ("color 2");
+		while (ciclos < 1000000)
+	{
+		cout << ",18.;#$%%&%/([A98750135464.'(57#%&10364'!2330/4910131604$(*/67210)/]><{|%6&772d10662";
+		ciclos = ciclos +1;	
+	} if ( c >= 48 && c <= 57)
+			putchar(c);
+    }
+	else
+	{
+	
+    	if(password=="neo"||password=="NEO")
+		{		
+        	system ("color 4");
+			while (ciclos <= 10)
+			{
+			cout << ",18.;#$%%&%/([A98750135464.'(57#%&10364'!2330/4910131604$(*/67210)/]><{|%6&772d10662";
+			ciclos = ciclos +1;
+			}
+			
+		}
+	}	cout << endl;
+		cout << endl;
+		system ("color 4");
+		cout<<"***EL SISTEMA FALLO***";
+		
+			
+			
+
+	return 0;
+}
