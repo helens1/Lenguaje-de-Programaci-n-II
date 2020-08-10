47f1f86c896881859b288e3879bee80ba52ab3db
Helen <hele.suarez@unah.hn>
2020-08-10 13:00:10 -0600

    archivo h

---
+++ EjerciciosDevC++/Ejercicio46-Archivos h/Alumnos.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+string nombre = "Juan";
+string apellido = "Perez";
+int edad = 18;
+
+string obtenerNombreCompleto()
+{
+	return nombre + " " + apellido; 
+}
+
+int obtenerEdad()
+{
+	return edad;
+}
