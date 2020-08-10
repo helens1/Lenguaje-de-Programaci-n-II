cf670c35c4381f02ab4a1d1cb07010ca45f0e8de
Helen <hele.suarez@unah.hn>
2020-08-10 13:04:12 -0600

    Funcion random

---
+++ EjerciciosDevC++/Ejercicio47-Funcion random/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+int main(int argc, char** argv) 
+{
+	int numero=0;
+	//inicializa el numero random
+	srand (time(NULL));
+	for (int i=0; i < 20; i ++)
+	{
+		//genera un numero entre 1 y 10
+	numero = rand()% 10 + 1;
+	cout << "primer numero al azar: " << numero << endl;
+	}
+		
+	return 0;
+}
